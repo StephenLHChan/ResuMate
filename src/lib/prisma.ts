988ac39da7e007eda936786288adab7667aea8fc
StephenLHChan <stephenc.1018@gmail.com>
2025-04-01 17:02:47 -0400

    feat(dependencies, prisma): update @auth/prisma-adapter to 2.7.2, add @neondatabase/serverless and @prisma/adapter-neon, and enhance Prisma client configuration with WebSocket support

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+neonConfig.poolQueryViaFetch = true;
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
